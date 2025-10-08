3420f2adf9e17f6ca420aef04ce2688d0ad1f246
hiraekram10 <heraiqram123@gmail.com>
2025-10-08 20:26:32 +0500

    function expression, map , object , array of object

---
+++ class21/index.js
@@ -0,0 +1,186 @@
+// function add (){
+//     console.log('hi');
+
+// }
+
+// add()
+
+// function expression
+//fat arrow function
+// let add = ()=>{
+//     console.log('hi');
+
+// }
+
+// add()
+
+
+
+// let add = (a,b)=> console.log(a+b);
+
+
+// add(3,4)
+
+
+// let add = a => console.log(`${a} Good Morning`);
+
+
+// add('sana')
+
+// function add (){
+//     return 'hi'
+// }
+
+
+// let check = add('sana')
+// console.log(check);
+
+
+// let add = ()=> 'hi'
+
+
+// let check = add('sana')
+// console.log(check);
+
+// array method
+// old method
+// let  arr = ['grapes','banna','apple']
+// for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+
+// }
+
+// Updated methodap
+
+// let  arr = ['grapes','banna','apple']
+// let returnCheck = arr.m((v,i)=>{
+//   return v
+
+// })
+// console.log(returnCheck);
+
+
+//map recieve callbackfn /alt for loop
+// callback fn 2 params reciev
+// 1. value
+// 2. index no
+// koi esa function jo dosray function main as an argument 
+// pass wo callbackfn
+
+
+// let main = document.querySelector('#main')
+// let  arr = ['grapes','banna','apple']
+//  arr.map((v,i)=>{
+//   main.innerHTML += `<li>${i}</li>`
+
+// })
+
+//  let main = document.querySelector('#main')
+//  let btn = document.querySelector('#btn')
+// // addEventListener 2 params
+// // 1. event trigger
+// 2// callbackfn
+//  btn.addEventListener('click',()=>{
+//     main.innerHTML = 'loremsjbdsdfhgsfhsghdfjbcm sdtfhwery geywtye iudy iuy  '
+
+//  })
+
+// let greeting = ()=>{
+
+// }
+
+// greeting()
+
+// Object 
+
+// let student1  = {
+//     fullName : 'waleed khan',  //property(key, value)
+//     rNo :  'gf4a',
+//     email :'waleed@gmail.com'
+// }
+
+// console.log(student1.rNo);
+
+
+// let student  = {
+//     fullName : 'waleed khan',  //property(key, value)
+//     rNo :  345345,
+//     email :'waleed@gmail.com',
+//     isPresent : true,
+//     subjects : ['html','css','js']
+
+// }
+// student.subjects.map((v,i)=>{
+//   console.log(v);
+
+// })
+
+
+// let student = {
+//     fullName: 'waleed khan',  //property(key, value)
+//     rNo: 345345,
+//     email: 'waleed@gmail.com',
+//     isPresent: true,
+//     personalInfo: {
+//         address: '14 gulshan',
+//         phone: 278354674
+//     }
+
+// }
+// console.log(student.address); // undefined
+// console.log(student.personalInfo.address);  //14 gulshan
+
+
+
+
+// let student = {
+//     fullName: 'waleed khan', 
+//     rNo: 345345,
+//     email: 'waleed@gmail.com',
+//     isPresent: true,
+//     personalInfo: {
+//         address: '14 gulshan',
+//         phone: 278354674
+//     }
+
+// }
+// let student2 = {
+//     fullName: 'waresha khan', 
+//     rNo: 345345,
+//     email: 'waresha@gmail.com',
+//     isPresent: false,
+//     personalInfo: {
+//         address: '14 gulshan',
+//         phone: 278354674
+//     }
+
+// }
+// let student3= {
+//     fullName: 'ali khan', 
+//     rNo: 3453423,
+//     email: 'ali@gmail.com',
+//     isPresent: true,
+//     personalInfo: {
+//         address: '14 gulshan',
+//         phone: 278354674
+//     }
+
+// }
+// let studentsData = [student,student2,student3]
+// studentsData.map((v,i)=>{
+//  console.log(v.fullName);
+ 
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
